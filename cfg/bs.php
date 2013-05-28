6868fc907e5155e27bed73c35d32a51b605e77fd
Rafael Prenzier <rafaelpernzier@gmail.com>
2013-05-28 13:04:57 -0300

    mais backups

--- cfg/bs.php
+++ cfg/bs.php
@@ -5,8 +5,8 @@ define("BD_BS", "ads");
 define("BD_HOST", "localhost");
 
 // MELI
-define("CLIENT_ID", "");
-define("CLIENT_SECRET", "");
+define("CLIENT_ID", "8396982338445035");
+define("CLIENT_SECRET", "1mPqyuXK954BBARAvPVFfz2S7AZjWUme");
 define("REDIRECT_URL", "http://localhost/softAds/login.php");
 define("BASE_URI", "https://api.mercadolibre.com");
 ?>
