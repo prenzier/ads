c4d77c4a8d003629b209c909fed3eb2aee20b825
Rafael Prenzier <rafaelpernzier@gmail.com>
2013-05-28 12:59:45 -0300

    recuperando backup

--- agencias.php
+++ agencias.php
@@ -1,1 +1,15 @@
-agenciasComplete(["Akrotiri"])
+<?
+require_once "classes/conn.php";
+$sql = query_("SELECT id, nome FROM agencias");
+
+$jsonp = "agenciasComplete(";
+while($agencias = mysql_fetch_object($sql)) {
+	$jsonp .= '[ id: "'.$agencias->id.'", nome: "'.$agencias->nome.'"],';
+}
+$jsonp = rtrim($jsonp, ",");
+$jsonp .= ")";
+echo $jsonp;
+
+
+#agenciasComplete(["Akrotiri"])
+?>
